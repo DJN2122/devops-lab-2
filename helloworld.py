db91e6f1c81fca74657c74906b24bb038178ef0f
David Niculita <david_niculita@yahoo.com>
2024-02-08 10:20:08 +0000

    Change greeting to Welcome!

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,6 @@
 # This program prints Hello, world!
 
-print('Hello, world!')
+print('Welcome!')
 
 print("Displaying numbers 1 to 10:")
 for i in range(1, 11):  # Start at 1, up to and including 10
